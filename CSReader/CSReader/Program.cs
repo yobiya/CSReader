6e48a2ae88ef39d57a5d7b46896d161ee50477c9
YoBiya <dummy@mail.com>
2016-12-25 00:48:35 +0900

    プロジェクトをVisualStudio2017用に変換

--- CSReader/CSReader/Program.cs
+++ CSReader/CSReader/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
-
-class Program
+﻿namespace CSReader
 {
-	static void Main(string[] args)
-	{
-		Console.WriteLine("Hello World!");
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
 }
