284380956e9fcd5af96255da87c16a45711d8f3b
YoBiya <dummy@mail.com>
2017-01-02 18:46:43 +0900

    サンプルプロジェクトの内容を追加

--- CSReader/TestTarget/Simple/Simple/Program.cs
+++ CSReader/TestTarget/Simple/Simple/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Simple
+﻿using System;
+
+namespace Simple
 {
+    /// <summary>
+    /// 単純なプロジェクト
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+			Print(1 + 2);
         }
+
+		private static void Print(int number)
+		{
+			Console.WriteLine("Sample print : " + number);
+		}
     }
 }
