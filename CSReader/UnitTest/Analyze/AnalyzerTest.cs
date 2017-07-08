920eeade6c00850682819c1cbf3542f069d376a6
Takeshi Yoshizawa <takeshiyoshizawa@TakeshinoMacBook-Pro.local>
2017-07-08 22:03:27 +0900

    解析のコードをそのテストを設定

---
+++ CSReader/UnitTest/Analyze/AnalyzerTest.cs
@@ -0,0 +1,21 @@
+﻿using CSReader.Analyze;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Analyze
+{
+    [TestClass]
+    public class AnalyzerTest
+    {
+        [TestMethod]
+        public void AnalyzeTest()
+        {
+			var analyzer = new Analyzer(GetSolutionPath("Simple"));
+			analyzer.Analyze();
+        }
+
+		private static string GetSolutionPath(string name)
+		{
+			return "../../../TestTarget/" + name + "/" + name + ".sln";
+		}
+    }
+}
