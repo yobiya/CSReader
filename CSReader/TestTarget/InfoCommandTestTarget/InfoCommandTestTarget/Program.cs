f597ab207b8537d50739380fe815116c2d61a004
Takeshi Yoshizawa <takeshiyoshizawa@TakeshinoMacBook-Pro.local>
2017-08-05 20:34:32 +0900

    InfoCommand用のテストプロジェクトを追加

---
+++ CSReader/TestTarget/InfoCommandTestTarget/InfoCommandTestTarget/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace InfoCommandTestTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printer = new Test1.Printer();
+            printer.Print("Test message.");
+        }
+    }
+}
---
+++ CSReader/TestTarget/InfoCommandTestTarget/InfoCommandTestTarget/Test1/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InfoCommandTestTarget.Test1
+{
+    public class Printer
+    {
+        public virtual void Print(string message)
+        {
+            PrintImpl(message);
+        }
+
+        public void PrintImpl(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
